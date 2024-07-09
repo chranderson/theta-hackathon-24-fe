a36dc6e12a27de30cbf2861d96ba8aa4021ef297
Chris Anderson <chranderson@users.noreply.github.com>
2024-07-09 13:31:56 -0600

    configures jest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+const title = process.env.NEXT_PUBLIC_APP_NAME;
+
 export default function Home() {
   return (
     <main className="flex-1 flex h-full flex-col items-center justify-center p-24">
       <h1 className="font-serif text-4xl text-center">
-        Theta Network Hackathon{' '}
+        {title}
         <span className="block text-primary text-6xl">2024</span>
       </h1>
     </main>
