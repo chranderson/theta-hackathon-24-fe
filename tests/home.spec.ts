f3f877f26b4bc975a1fe77d2c5ae0e55131714d7
Chris Anderson <chranderson@users.noreply.github.com>
2024-07-10 11:58:05 -0600

    adds home e2e test

---
+++ tests/home.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Team Theta/);
+});
+
+test('has description', async ({ page }) => {
+  await page.goto('/');
+  const metaDescription = page.locator('meta[name="description"]');
+  await expect(metaDescription).toHaveAttribute(
+    'content',
+    /Theta Network Hackathon 2024 entry/
+  );
+});
+
+test('heading', async ({ page }) => {
+  await page.goto('/');
+  await expect(
+    page.getByRole('heading', { name: 'Theta Network Hackathon' })
+  ).toBeVisible();
+});
+
+test('Connect Wallet button', async ({ page }) => {
+  await page.goto('/');
+  await expect(
+    page.getByRole('button', { name: 'Connect Wallet' })
+  ).toBeVisible();
+});
