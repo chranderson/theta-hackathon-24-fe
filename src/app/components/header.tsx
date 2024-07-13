859899b754b5a1e84d5b28f0063fd193d4f3ea01
Chris Anderson <chranderson@users.noreply.github.com>
2024-07-13 15:06:00 -0600

    removes header outline

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,7 +4,7 @@ import { env } from '@/lib/env';
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-border/40 dark:bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
-      <div className="px-4 sm:container flex h-14 max-w-screen-2xl items-center outline justify-between">
+      <div className="px-4 sm:container flex h-14 max-w-screen-2xl items-center justify-between">
         <div className="mr-4 md:flex">
           <a className="mr-4 flex items-center space-x-2 lg:mr-6" href="/">
             <span className="font-bold lg:inline-block">
